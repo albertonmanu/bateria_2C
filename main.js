0453c8fabe29a3133ef308f0ae775fe96d0c1b4b
albertonmanu <131890872+albertonmanu@users.noreply.github.com>
2023-11-10 09:32:26 -0300

    fr

--- main.js
+++ main.js
@@ -3,7 +3,3 @@ function tocaSomPom (){
     }
     document.querySelector('.tecla_clap') .onclick = tocaSomclap;
 
-    function tocaSomclap(){
-        document.querySelector('#som_tecla_clap').play();
-        }
-        document.querySelector('.tecla_clap') .onclick = tocaSomclap;
